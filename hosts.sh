8c32c40e884656cf2349225cd15d37ee1a9441dd
N36L3C7 <31883347+n36l3c7@users.noreply.github.com>
2023-08-28 21:31:31 +0200

    Feat: Add operations name
    Initialized the operations that will be performed by the script

--- hosts.sh
+++ hosts.sh
@@ -3,3 +3,22 @@
 TEMP_HOSTS="/etc/tmp_hosts"
 HOSTS="/etc/hosts"
 
+# CREATE TMP FILE
+cp $HOSTS $TEMP_HOSTS
+
+# OPERATION
+OPERATION=$1
+shift
+
+if [ $OPERATION = "add" ]; then
+    echo "add"
+elif [ $OPERATION = "append" ]; then
+    echo "append"
+elif [ $OPERATION = "pop" ]; then
+    echo "pop"
+elif [ $OPERATION = "remove" ]; then
+    echo "remove"
+else
+    echo "usage"
+    exit 0
+fi
